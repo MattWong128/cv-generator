59fe499a4bab34a82c02dd28238d1fcc964844b9
matthew Wong <matthew.wong128@gmail.com>
2024-08-15 13:55:21 -0400

    added mroe fields

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState, version } from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
+  const [title, setTitle] = useState();
   const [number, setNumber] = useState();
   const [location, setLocation] = useState();
+  const [summary, setSummary] = useState();
 
   return (
     <div className='container'>
@@ -13,10 +16,17 @@ function App() {
         <h1>Resume Generator</h1>
       </div>
       <div className='user-input'>
-        <div className='personal-details'>
-          <h2>Personal Details</h2>
-          <h4>Name</h4>
-          <input type='text' value={name} onChange={(event) => setName(event.target.value)} />
+        <div className='Personal Information'>
+          <h4>First name</h4>
+          <input type='text' value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+          <h4>Last Name</h4>
+          <input type='text' value={lastName} onChange={(event) => setLastName(event.target.value)} />
+          <h4>Title</h4>
+          <input type='text' value={title} onChange={(event) => setTitle(event.target.value)} />
+        </div>
+        <div className='contact'>
+          <h2>Contact Information</h2>
+
           <h4>Email</h4>
           <input type='email' value={email} onChange={(event) => setEmail(event.target.value)} />
           <h4>Phone number</h4>
@@ -26,6 +36,7 @@ function App() {
         </div>
         <div>
           <h2>Summary</h2>
+          <input type='text' value={summary} onChange={(event) => setSummary(event.target.value)} />
         </div>
         <div>
           <h2>Education</h2>
@@ -37,11 +48,17 @@ function App() {
           <h2>Skills</h2>
         </div>
         <div>
-          <h2>Certifications</h2>
+          <h2>Langauges</h2>
+        </div>
+        <div>
+          <h2>References</h2>
         </div>
       </div>
 
-      <div className='resume-container'></div>
+      <div className='resume-container'>
+        <div className='left-side'></div>
+        <div className='right-side'></div>
+      </div>
     </div>
   );
 }
